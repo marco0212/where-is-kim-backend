e4eeda36359cb224072e8ffcf40ac9f81b783eb9
Jeong <inyeop0212@gmail.com>
2020-04-03 18:06:19 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.end('Hello?');
+});
+
+module.exports = app;
