1240871151c4c1d180bd43d0a670fd0cadc3e930
Jeong <inyeop0212@gmail.com>
2020-04-03 18:25:31 +0900

    Connect DB

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const app = express();
 
+import './db';
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
