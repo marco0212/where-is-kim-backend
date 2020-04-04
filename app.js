6eb3cf2cc6a5b4515bd994c87d4d87e0dd0c71a1
Jeong <inyeop0212@gmail.com>
2020-04-04 20:22:06 +0900

    Complete /api/auth/signup

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import logger from 'morgan';
 import cors from 'cors';
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
+import User from './model/user';
 
 const app = express();
 
@@ -39,6 +40,30 @@ app.post('/api/auth/login', (req, res) => {
        return res.json({ token });
     });
   })(req, res);
+});
+
+app.post('/api/auth/signup', async (req, res) => {
+  const {
+    username,
+    email,
+    profilePic,
+    password,
+    confirmPassword
+  } = req.body;
+
+  try {
+    const user = new User({
+      username,
+      email,
+      profilePic
+    });
+
+    await User.register(user, password);
+
+    res.json({ result: 'ok' });
+  } catch (error) {
+    return res.json({ result: 'error', error });
+  }
 });
 
 module.exports = app;
