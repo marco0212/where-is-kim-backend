8697a43b4ee06589808497815649ae753765b4d5
Jeong <inyeop0212@gmail.com>
2020-04-06 18:28:54 +0900

    Fix Login logic

--- app.js
+++ app.js
@@ -1,71 +1,64 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import cors from 'cors';
-import passport from 'passport';
-import jwt from 'jsonwebtoken';
-import User from './model/user';
-
-dotenv.config();
+import express from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from "cors";
+import passport from "passport";
+import jwt from "jsonwebtoken";
+import User from "./model/user";
 
 const app = express();
 
-import './db';
-import './passport';
+import "./db";
+import "./passport";
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
 
-app.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.post("/", passport.authenticate("jwt", { session: false }), (req, res) => {
   res.json(req.user);
 });
 
-app.post('/api/auth/login', (req, res) => {
-  passport.authenticate('local', { session: false }, (err, user) => {
+app.post("/api/auth/login", (req, res) => {
+  passport.authenticate("local", { session: false }, (err, user) => {
     if (err) {
-      return res.json({ result: 'error', err });
+      res.status(400);
+      return res.json({ result: "error", err });
     }
 
     if (!user) {
-      return res.json({ result: 'wrong account' });
+      res.status(400);
+      return res.json({ result: "wrong account" });
     }
 
     req.login(user, { session: false }, (err) => {
-       if (err) return res.send(err);
+      if (err) return res.json(err);
 
-       const payload = { id: user.id };
-       const token = jwt.sign(payload, process.env.JWT_SECRET);
+      const payload = { id: user.id };
+      const token = jwt.sign(payload, process.env.JWT_SECRET);
 
-       return res.json({ result: { token, user } });
+      return res.json({ result: { token, user } });
     });
   })(req, res);
 });
 
-app.post('/api/auth/signup', async (req, res) => {
-  const {
-    username,
-    email,
-    profilePic,
-    password,
-    confirmPassword
-  } = req.body;
+app.post("/api/auth/signup", async (req, res) => {
+  const { username, email, password } = req.body;
 
   try {
     const user = new User({
       username,
       email,
-      profilePic
     });
 
     await User.register(user, password);
 
-    res.json({ result: 'ok' });
+    res.json({ result: "ok" });
   } catch (error) {
-    return res.json({ result: 'error', error });
+    return res.json({ result: "error", error });
   }
 });
 
--- passport.js
+++ passport.js
@@ -1,42 +1,40 @@
-import passport from 'passport';
-import passportLocal from 'passport-local';
-import User from './model/user';
-import passportJwt from 'passport-jwt'
+import dotenv from "dotenv";
+import passport from "passport";
+import passportLocal from "passport-local";
+import User from "./model/user";
+import passportJwt from "passport-jwt";
 
+dotenv.config();
 const LocalStrategy = passportLocal.Strategy;
 const JwtStrategy = passportJwt.Strategy;
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email", passwordField: "password" },
+    User.authenticate()
+  )
+);
+
 const jwtOptions = {
   jwtFromRequest: passportJwt.ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret'
+  secretOrKey: process.env.JWT_SECRET,
 };
 
-passport.use(new LocalStrategy(
-  { usernameField: 'email' },
-  async (email, password, done) => {
+passport.use(
+  new JwtStrategy(jwtOptions, async (payload, done) => {
     try {
-      const user = await User.findOne({ email });
+      const user = await User.findById(payload.id);
 
-      if (!user) {
-        return done(null, false);
+      if (user) {
+        done(null, user);
+      } else {
+        done(null, false);
       }
-
-      return done(null, user);
     } catch (err) {
-      return done(err);
+      done(err);
     }
-  }
-));
+  })
+);
 
-passport.use(new JwtStrategy(jwtOptions, async (payload, done) => {
-  try {
-    const user = await User.findById(payload.id);
-
-    if (user) {
-      done(null, user);
-    } else {
-      done(null, false);
-    }
-  } catch (err) {
-    done(err);
-  }
-}));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
