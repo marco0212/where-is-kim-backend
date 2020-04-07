71d1b9ec67893370e08c737c2dec7fc165955f50
Jeong <inyeop0212@gmail.com>
2020-04-07 17:12:53 +0900

    Add sample sending mail API

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import cors from "cors";
 import passport from "passport";
 import auth from "./api/auth";
 import team from "./api/team";
+import { sendMail } from "./utils";
+
 const app = express();
 
 import "./db";
@@ -19,5 +21,18 @@ app.use(passport.initialize());
 
 app.use("/api/auth", auth);
 app.use("/api/team", team);
+
+// Sample API sending Mail to test
+
+app.post("/mail", async (req, res) => {
+  try {
+    const result = await sendMail();
+
+    res.json({ result });
+  } catch (err) {
+    res.status(500);
+    res.json({ result: "error", err });
+  }
+});
 
 module.exports = app;
