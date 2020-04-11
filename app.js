e3b40764ffada56c769e811107283fe332c00ebf
Jeong <inyeop0212@gmail.com>
2020-04-11 16:29:53 +0900

    Add leave team handler

--- app.js
+++ app.js
@@ -55,6 +55,28 @@ io.on("connection", (socket) => {
 
     io.in(teamName).emit("join team", participants);
   });
+
+  socket.on("leave team", leaveTeam);
+  socket.on("disconnect", leaveTeam);
+
+  function leaveTeam() {
+    const roomName = roomById[id];
+
+    if (roomName) {
+      const room = roomByName[roomName];
+      const index = room.indexOf(id);
+
+      room.splice(index, 1);
+      delete socketById[id];
+      delete userIdById[id];
+      delete roomById[id];
+
+      const participants = room.map((id) => userIdById[id]);
+
+      socket.to(roomName).broadcast.emit("leave team", participants);
+      socket.leave(roomName);
+    }
+  }
 });
 
 module.exports = server;
