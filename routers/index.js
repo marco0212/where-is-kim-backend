02b8f8b0cf63ec3d8dc51df50b1ff645a157ee54
Jeong <inyeop0212@gmail.com>
2020-05-16 18:25:17 +0900

    Apply JWT Authenticate

--- routers/index.js
+++ routers/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { authenticateWithJWT } from "../middlewares";
 import auth from "./auth";
 import team from "./team";
 import thread from "./thread";
@@ -6,7 +7,7 @@ import thread from "./thread";
 const router = express.Router();
 
 router.use("/auth", auth);
-router.use("/team", team);
-router.use("/thread", thread);
+router.use("/team", authenticateWithJWT, team);
+router.use("/thread", authenticateWithJWT, thread);
 
 export default router;
