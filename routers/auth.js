997e4d56449e8abd3f919f52b689bdb536129239
Jeong <inyeop0212@gmail.com>
2020-05-21 12:51:58 +0900

    Linting routers

--- routers/auth.js
+++ routers/auth.js
@@ -1,10 +1,10 @@
-import express from "express";
-import { upload } from "../middlewares";
-import { login, signup } from "../controllers/auth";
+import express from 'express';
+import { upload } from '../middlewares';
+import { login, signup } from '../controllers/auth';
 
 const router = express.Router();
 
-router.post("/login", login);
-router.post("/signup", upload.single("profile"), signup);
+router.post('/login', login);
+router.post('/signup', upload.single('profile'), signup);
 
 export default router;
